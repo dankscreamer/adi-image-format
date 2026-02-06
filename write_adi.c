6c3badffc699120898f6de4f3b355bbdf6d7fb5d
Adit Singh <aditatpeace@gmail.com>
2026-02-06 16:57:52 +0530

    added full write code

--- write_adi.c
+++ write_adi.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 
-struct ADIheader{
+struct ADIHeader {
     char magic[3];
     int width;
-    int height; 
+    int height;
     unsigned char channels;
-    }
+};
 
 int main(void) {
     int width = 2;
     int height = 2;
     unsigned char channels = 3;
 
-    unsigned char image[(width * height) * channels] = {
+    unsigned char image[12] = {
         255, 242, 222,
         255, 242, 244,
         255, 200, 242,
         244, 222, 244
+    };
+
     struct ADIHeader header;
     header.magic[0] = 'A';
     header.magic[1] = 'D';
@@ -24,6 +26,19 @@ int main(void) {
     header.width = width;
     header.height = height;
     header.channels = channels;
-       return 0;
+
+
+    //file writing
+    FILE *fptr=fopen("test.adi","wb");
+    if (fptr==NULL){
+        return 1;}
+    fwrite(&header,sizeof(header),1,fptr);
+    fwrite(image,1,(width * height) * channels,fptr);
+    fclose(fptr);
+
+
+    
+
+    return 0;
 }
 
